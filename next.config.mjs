e9eaab661b54915724ab4b58e0cfcba99edc4cd8
KeenCoderIsHere <susheethvenkatraman@gmail.com>
2026-03-28 18:08:13 +0530

    FInal UI fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    output: 'export',
+   images: {
+    unoptimized: true,   // ← add this line
+  },
   /* config options here */
 };
 
